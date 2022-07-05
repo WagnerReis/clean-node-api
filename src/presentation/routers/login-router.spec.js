6767157c743010f4a4e238b9d4e530e2940dd480
WagnerReis <wagnerreis1997@gmail.com>
2022-07-05 09:37:42 -0300

    move errors to a custom folger

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,8 +1,11 @@
-const MissingParamError = require('../helpers/missing-param-error')
-const InvalidParamError = require('../helpers/invalid-param-error')
-const ServerError = require('../helpers/server-error')
-const UnauthorizedError = require('../helpers/unauthorized-error')
 const LoginRouter = require('./login-router')
+
+const {
+  MissingParamError,
+  InvalidParamError,
+  ServerError,
+  UnauthorizedError
+} = require('../errors')
 
 const makeSut = () => {
   const authUseCaseSpy = makeAuthUseCase()
