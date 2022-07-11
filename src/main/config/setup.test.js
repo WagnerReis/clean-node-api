a96a33397e841f4ce60199e44191fc5b12212e2a
WagnerReis <wagnerreis1997@gmail.com>
2022-07-11 14:15:54 -0300

    enable CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -3,12 +3,22 @@ const app = require('./app')
 
 describe('App Setup', () => {
   test('Should disable x-powered-by header', async () => {
-    await request(app)
-      .get('/test_x_powered_by', (req,res) => {
-        res.send('')
-      })
+    app.get('/test_x_powered_by', (req,res) => {
+      res.send('')
+    })
 
     const res = await request(app).get('/test_x_powered_by')
     expect(res.headers['x_powered_by']).toBeUndefined()
+  })
+
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (req,res) => {
+      res.send('')
+    })
+
+    const res = await request(app).get('/test_cors')
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
   })
 })
