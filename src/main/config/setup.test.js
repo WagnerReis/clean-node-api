01cd101b0a9b44b8e135d924983f9e6437a64175
WagnerReis <wagnerreis1997@gmail.com>
2022-07-11 13:54:12 -0300

    disable e-powered-by header

---
+++ src/main/config/setup.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest')
+const app = require('./app')
+
+describe('App Setup', () => {
+  test('Should disable x-powered-by header', async () => {
+    await request(app)
+      .get('/test_x_powered_by', (req,res) => {
+        res.send('')
+      })
+
+    const res = await request(app).get('/test_x_powered_by')
+    expect(res.headers['x_powered_by']).toBeUndefined()
+  })
+})
